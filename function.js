52872992c057d919979adf0ffb16c8454158a687
Diego Grandon <118071648+grandon13@users.noreply.github.com>
2022-11-29 11:46:40 -0300

    Color cells grey when the mouse hovers over them

--- function.js
+++ function.js
@@ -1,10 +1,17 @@
-let size = 4;
+let gridSize = 100;
 const sketchGrid = document.querySelector('.workspace');
-sketchGrid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
-for(let i = 0; i < size ** 2; i++) {
-    let brick = document.createElement('div');
-    brick.setAttribute('id', `brick${i}`);
-    brick.setAttribute('class', 'brick');
-    sketchGrid.appendChild(brick);
+//Builds the grid based on the gridSize number
+sketchGrid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+for(let i = 0; i < gridSize ** 2; i++) {
+    let cell = document.createElement('div');
+    cell.setAttribute('id', `cell${i}`);
+    cell.setAttribute('class', 'cell');
+    sketchGrid.appendChild(cell);
 }
+
+//Colors cells when the mouse hovers over them
+const cells = document.querySelectorAll('.cell');
+cells.forEach(brick => brick.addEventListener('mouseover', () => {
+    brick.style.backgroundColor = 'grey';
+}));

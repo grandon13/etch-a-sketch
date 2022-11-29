d87990512dfe1cc70212bed4defc3fd78a453456
Diego Grandon <118071648+grandon13@users.noreply.github.com>
2022-11-29 14:54:32 -0300

    Finish the original project
    The final version is as simple as possible, that's because I wanted
    to focus exclusively in javascript. The project does everything that
    was asked as I understood it.

--- function.js
+++ function.js
@@ -1,4 +1,5 @@
 let gridSize = 16;
+let colorCellInfo;
 const sketchGrid = document.querySelector('.workspace');
 
 buildGrid(gridSize);
@@ -6,17 +7,15 @@ colorGrid();
 
 //RGB seemed the easiest way to randomize a color
 function getRandomRGB() {
-    let red, green, blue;
-    let rgbColor;
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255 );
+    let blue = Math.floor(Math.random() * 255 );
 
-    red = Math.random() * 255;
-    green = Math.floor(Math.random() * 255 );
-    blue = Math.floor(Math.random() * 255 );
-
-    rgbColor = `rgb(${red}, ${green}, ${blue})`;
+    let rgbColor = [red, green, blue];
     return rgbColor;
 }
 
+//Receives input from user and sets a new grid
 function setGridSize() {
     gridSize = prompt('Choose a grid size between 1 and 100');
 
@@ -30,12 +29,15 @@ function setGridSize() {
 
 //Builds the grid based on the gridSize number
 function buildGrid(gridSize) {
+    colorCellInfo = new Array(gridSize**2);
+
     sketchGrid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     for(let i = 0; i < gridSize ** 2; i++) {
         let cell = document.createElement('div');
         cell.setAttribute('id', `cell${i}`);
         cell.setAttribute('class', 'cell');
         sketchGrid.appendChild(cell);
+
     }
 }
 
@@ -50,22 +52,31 @@ function colorGrid() {
     for (let i = 0; i < cells.length; i++) {
         cells[i].addEventListener('mouseover', () => {
             if (! cells[i].classList.contains('colored')) {
-                cells[i].style.backgroundColor = getRandomRGB();
+                let rgb = getRandomRGB();
+                colorCellInfo[i] = [rgb[0], rgb[1], rgb[2], 0];
+                cells[i].style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
                 cells[i].classList.toggle('colored');
             }
             else {
-                dimColor(cells[i].style.backgroundColor);
+                dimColor(colorCellInfo[i], i);
             }
         });
     }
 }
 
-function dimColor(rgb) {
-    rgb = rgb.slice(4, -1);
-    let rgbArray = rgb.split(', ').map(str => {
-        return Number(str);
-    });
-    console.log(rgbArray);
+function dimColor(colorArray, index) {
+    const cells = document.querySelectorAll('.cell');
+
+    if (colorArray[3] < 10) {
+        colorCellInfo[index][0] = Math.floor(colorArray[0] - colorArray[0] / (10 - colorArray[3]));
+        colorCellInfo[index][1] = Math.floor(colorArray[1] - colorArray[1] / (10 - colorArray[3]));
+        colorCellInfo[index][2] = Math.floor(colorArray[2] - colorArray[2] / (10 - colorArray[3]));
+        colorCellInfo[index][3]++;
+
+        cells[index].style.backgroundColor = `rgb(${colorCellInfo[index][0]},
+             ${colorCellInfo[index][1]},
+              ${colorCellInfo[index][2]})`;
+    }
 }
 
 const gridBtn = document.querySelector('.setGrid');

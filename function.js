3e1a126f9d0db36bce8dd9537b59a3a920b45db9
Diego Grandon <118071648+grandon13@users.noreply.github.com>
2022-11-29 11:57:49 -0300

    Randomize the color instead of just grey

--- function.js
+++ function.js
@@ -1,5 +1,19 @@
-let gridSize = 100;
+let gridSize = 16;
 const sketchGrid = document.querySelector('.workspace');
+
+//RGB seemed the easiest way to randomize a color
+function randomRGB() {
+    let red, green, blue;
+    let rgbColor;
+
+    red = Math.floor(Math.random() * 255 );
+    green = Math.floor(Math.random() * 255 );
+    blue = Math.floor(Math.random() * 255 );
+
+    rgbColor = `rgb(${red}, ${green}, ${blue})`;
+    return rgbColor;
+}
+randomRGB();
 
 //Builds the grid based on the gridSize number
 sketchGrid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
@@ -13,5 +27,5 @@ for(let i = 0; i < gridSize ** 2; i++) {
 //Colors cells when the mouse hovers over them
 const cells = document.querySelectorAll('.cell');
 cells.forEach(brick => brick.addEventListener('mouseover', () => {
-    brick.style.backgroundColor = 'grey';
+    brick.style.backgroundColor = randomRGB();
 }));

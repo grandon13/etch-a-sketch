e1af519357d6090be3fe19a53520f102de0b15f9
Diego Grandon <118071648+grandon13@users.noreply.github.com>
2022-11-29 12:55:54 -0300

    Activate grid size button
    Doing this was harder than it originally looked. I had to create
    two extra functions, one to remove the cells in the grid and one
    that activates the color once again. All in all, this was probably the
    most difficult part of this project, at least so far.

--- function.js
+++ function.js
@@ -1,8 +1,11 @@
 let gridSize = 16;
 const sketchGrid = document.querySelector('.workspace');
 
+buildGrid(gridSize);
+colorGrid();
+
 //RGB seemed the easiest way to randomize a color
-function randomRGB() {
+function getRandomRGB() {
     let red, green, blue;
     let rgbColor;
 
@@ -13,19 +16,42 @@ function randomRGB() {
     rgbColor = `rgb(${red}, ${green}, ${blue})`;
     return rgbColor;
 }
-randomRGB();
+
+function setGridSize() {
+    gridSize = prompt('Choose a grid size between 1 and 100');
+
+    while (gridSize < 1 || gridSize > 100) {
+        gridSize = prompt('Invalid number! Please choose a grid size between 1 and 100');
+    }
+    removeGrid();
+    buildGrid(gridSize);
+    colorGrid();
+}
 
 //Builds the grid based on the gridSize number
-sketchGrid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-for(let i = 0; i < gridSize ** 2; i++) {
-    let cell = document.createElement('div');
-    cell.setAttribute('id', `cell${i}`);
-    cell.setAttribute('class', 'cell');
-    sketchGrid.appendChild(cell);
+function buildGrid(gridSize) {
+    sketchGrid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    for(let i = 0; i < gridSize ** 2; i++) {
+        let cell = document.createElement('div');
+        cell.setAttribute('id', `cell${i}`);
+        cell.setAttribute('class', 'cell');
+        sketchGrid.appendChild(cell);
+    }
+}
+
+function removeGrid() {
+    sketchGrid.replaceChildren();
 }
 
 //Colors cells when the mouse hovers over them
-const cells = document.querySelectorAll('.cell');
-cells.forEach(brick => brick.addEventListener('mouseover', () => {
-    brick.style.backgroundColor = randomRGB();
-}));
+function colorGrid() {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(brick => brick.addEventListener('mouseover', () => {
+        brick.style.backgroundColor = getRandomRGB();
+    }));
+}
+
+const gridBtn = document.querySelector('.setGrid');
+gridBtn.addEventListener('click', () => {
+    setGridSize();
+});

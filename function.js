f18af66a1cd8d63aac23909308a03f3422f1941b
Diego Grandon <118071648+grandon13@users.noreply.github.com>
2022-11-29 10:25:32 -0300

    Solve customizable grid
    At this stage there's only a very basic design, but the most important
    thing, which is the customizable grid, is solved. I managed to
    manipulate the grid from javascript.

--- function.js
+++ function.js
@@ -1,2 +1,10 @@
-const container = document.querySelector('div');
-container.textContent = "Te cambié csm!";
+let size = 4;
+const sketchGrid = document.querySelector('.workspace');
+sketchGrid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+for(let i = 0; i < size ** 2; i++) {
+    let brick = document.createElement('div');
+    brick.setAttribute('id', `brick${i}`);
+    brick.setAttribute('class', 'brick');
+    sketchGrid.appendChild(brick);
+}
